ea7a7586709a4c2b787bf6519bf848443f89232d
Yuri March <78003803+YuriMarch@users.noreply.github.com>
2023-01-27 21:52:41 +0100

    jwt authentication added

--- src/main/java/com/distancelearning/notification/controllers/UserNotificationController.java
+++ src/main/java/com/distancelearning/notification/controllers/UserNotificationController.java
@@ -1,15 +1,20 @@
 package com.distancelearning.notification.controllers;
 
+import com.distancelearning.notification.configs.security.UserDetailsImpl;
 import com.distancelearning.notification.dtos.NotificationDto;
 import com.distancelearning.notification.models.NotificationModel;
 import com.distancelearning.notification.services.NotificationService;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,17 +24,23 @@ import java.util.UUID;
 @RestController
 @AllArgsConstructor
 @CrossOrigin(origins = "*", maxAge = 3600)
+@Log4j2
 public class UserNotificationController {
 
     private final NotificationService notificationService;
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/users/{userId}/notifications")
     public ResponseEntity<Page<NotificationModel>> getAllNotificationsByUser(
             @PathVariable(value="userId") UUID userId,
-            @PageableDefault(page = 0, size = 5, sort = "notificationId", direction = Sort.Direction.ASC) Pageable pageable){
+            @PageableDefault(page = 0, size = 5, sort = "notificationId", direction = Sort.Direction.ASC) Pageable pageable,
+            Authentication authentication){
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        log.info("UserId authenticated: {}", userDetails.getUserId());
         return ResponseEntity.status(HttpStatus.OK).body(notificationService.findAllNotificationsByUser(userId, pageable));
     }
 
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @PutMapping("/users/{userId}/notifications/{notificationId}")
         public ResponseEntity<Object> updateNotificationStatus(@PathVariable(value="userId") UUID userId,
                                                                @PathVariable(value="notificationId") UUID notificationId,
